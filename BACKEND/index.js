bae7f8bcbe29b9c378231796e9b71c98c9af44f1
SHUBHAM DAS <149561473+ShubhamDaS-IIT-Dhanbad@users.noreply.github.com>
2024-05-18 00:53:51 +0530

    Update index.js

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -8,11 +8,9 @@ dotenv.config({
 const port=process.env.PORT || 4000;
 connectDB()
 .then(() => {
-<<<<<<< HEAD
     app.listen(process.env.PORT || 4000, () => {
-=======
+
     app.listen(port, () => {
->>>>>>> 66a1195f0d56da5db8d65b1d432937fdac7e9e67
         console.log(`Server is running at port : ${process.env.PORT}`);
     })
 })
